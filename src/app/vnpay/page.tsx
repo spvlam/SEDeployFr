46ee3943e8af5eaff798474428c9094763c8a2fb
lamnv10 <batdongsanvinhphuc12@gmail.com>
2024-06-17 16:28:46 +0700

    build

--- src/app/vnpay/page.tsx
+++ src/app/vnpay/page.tsx
@@ -1,20 +1,26 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
 import { PARAMETER_USER } from '@/config/API_config';
-export default function vnpay(){
-    const searchParams = useSearchParams()
-  
-  const code= searchParams.get(PARAMETER_USER.vnPay) ;
-  console.log(code)
+import { useState, useEffect } from 'react';
 
-  
+export default function Vnpay() {
+  const [code, setCode] = useState<string | null>('00');
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const userCode = searchParams.get(PARAMETER_USER.vnPay);
+    if (userCode) {
+      setCode(userCode);
+    }
+  }, [searchParams]);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h2 className="text-3xl font-semibold mb-4">
-        {code === '00' || '02' ? 'Success Payment' : 'Failure Payment'}
+        {code === '00' || code === '02' ? 'Success Payment' : 'Failure Payment'}
       </h2>
       <div className="bg-gray-200 dark:bg-gray-800 p-6 rounded-lg shadow-md">
-        {code === '00' || '02' ? (
+        {code === '00' || code === '02' ? (
           <p className="text-green-600">Your payment was successful.</p>
         ) : (
           <p className="text-red-600">There was an issue with your payment.</p>
